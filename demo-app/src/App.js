7b459e31960a3d30d940dd75e602dcb29c9a8123
besimahaa <71436746+besimahaa@users.noreply.github.com>
2022-05-12 16:39:30 +0300

    adddemo

---
+++ demo-app/src/App.js
@@ -0,0 +1,37 @@
+import {BrowserRouter as Router,Switch,Route,Link} from "react-router-dom";
+
+import Home  from "./components/Home";
+import About  from "./components/About";
+import Users  from "./components/Users";
+
+
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/users" component={Users}/>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
